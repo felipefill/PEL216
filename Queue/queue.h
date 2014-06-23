d348236cd90b5ebf4ad919b29e1d949c8f1d9aa2
Felipe Fill Cardoso <github@felipefill.com>
2014-06-23 01:13:56 -0300

    Template Queue class

--- Queue/queue.h
+++ Queue/queue.h
@@ -1,15 +1,20 @@
 #ifndef QUEUE_H
 #define QUEUE_H
 
+template <class T>
 class Queue
 {
 private:
     class QueueElement {
       public:
-        int value;
+        T *value;
         QueueElement *next;
 
-        QueueElement(int value, QueueElement *next);
+        inline QueueElement(T *value, QueueElement *next)
+        {
+            this->value = value;
+            this->next = next;
+        }
     };
 
     QueueElement *first;
@@ -17,12 +22,61 @@ private:
     int size;
 
 public:
-    Queue();
+    inline Queue()
+    {
+        this->first = NULL;
+        this->last = NULL;
+        this->size = 0;
+    }
 
-    bool isEmpty();
-    int getSize();
-    void queue(int value);
-    int dequeue(void);
+    inline bool isEmpty()
+    {
+        return this->size == 0;
+    }
+
+    inline int getSize()
+    {
+        return this->size;
+    }
+
+    inline void queue(T *value)
+    {
+        QueueElement *newElement = new QueueElement(value, NULL);
+
+        if (this->isEmpty()) {
+            this->first = newElement;
+        }
+        else {
+            this->last->next = newElement;
+        }
+
+        this->last = newElement;
+
+        this->size++;
+    }
+
+    inline T *dequeue()
+    {
+        T *value = NULL;
+
+        if (this->getSize() > 0) {
+            QueueElement *first = this->first;
+            value = first->value;
+
+            if (first == this->last) {
+                this->first = NULL;
+                this->last = NULL;
+            }
+            else {
+                this->first = first->next;
+            }
+
+            delete first;
+            this->size--;
+        }
+
+        return value;
+    }
 };
 
 #endif // QUEUE_H
