1b12aefea413601567fc94cb5a7ec1de56a93905
Felipe Fill Cardoso <felipe.cardoso@taqtile.com>
2014-08-04 18:38:27 -0300

    fixing Best-first

--- EightPuzzle/EightPuzzle/source/best_first_search.cc
+++ EightPuzzle/EightPuzzle/source/best_first_search.cc
@@ -16,17 +16,18 @@ namespace eightPuzzle
     
     std::vector<PuzzleState *> BestFirstSearch::DoSearch(PuzzleState *initial_state, PuzzleState *goal_state)
     {
-        Stack<PuzzleState> states_to_visit = Stack<PuzzleState>();
+        std::vector<PuzzleState *> states_to_visit;
         std::vector<PuzzleState *> visited_states;
         
-        states_to_visit.Push(initial_state);
+        states_to_visit.push_back(initial_state);
         visited_states.push_back(initial_state);
         
         int highest_score = 0;
         
         PuzzleState * current_state;
-        while (!states_to_visit.IsEmpty()) {
-            current_state = states_to_visit.Pop();
+        while (!states_to_visit.empty()) {
+            current_state = states_to_visit.back();
+            states_to_visit.pop_back();
             
             int score = Puzzle::EvaluateScoreForState(current_state);
             if (score > highest_score) {
@@ -40,18 +41,18 @@ namespace eightPuzzle
             
             std::vector<PuzzleState *> children = current_state->Children();
             
-            // This ordering will garantee that we have the highest score always on top of the stack
-            Puzzle::OrderByScoreAscending(children);
-            
             for (int i = 0; i < children.size(); i++) {
                 if (!ArrayContainsState(visited_states, children.at(i))) {
-                    states_to_visit.Push(children.at(i));
+                    states_to_visit.push_back(children.at(i));
                     visited_states.push_back(children.at(i));
                 }
                 else {
                     delete children.at(i);
                 }
             }
+            
+            // This ordering will garantee that we have the highest score always on top of the stack
+            Puzzle::OrderByScoreAscending(states_to_visit);
             
         }
         
